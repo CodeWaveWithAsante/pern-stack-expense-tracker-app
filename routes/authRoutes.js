4b453a5e157d88b5f8487e5d4d2a83d788253c73
Codewave <132752907+CodeWaveWithAsante@users.noreply.github.com>
2024-09-15 19:36:07 +0530

    Delete routes directory

--- routes/authRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { signinUser, signupUser } from "../controllers/authController.js";
-
-const router = express.Router();
-
-router.post("/sign-up", signupUser);
-router.post("/sign-in", signinUser);
-
-export default router;
